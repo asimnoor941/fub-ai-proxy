529b7eefc76020de2ac6406e2366f1646372c289
asimnoor941 <asim.noor941@gmail.com>
2025-10-05 22:53:09 -0700

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import fetch from "node-fetch";
+const app = express();
+
+app.get("*", async (req, res) => {
+  try {
+    const target =
+      "https://script.google.com/macros/s/AKfycbymqHP6PzaGyIfh6yCydE17THEZEiASGpWJ13uUPNvJ0VUKRwW9q-Gm77hpgIeZy5a7jQ/exec" +
+      req.url;
+    const r = await fetch(target);
+    const text = await r.text();
+    res.set("Content-Type", "text/html");
+    res.removeHeader("X-Frame-Options");
+    res.removeHeader("Content-Security-Policy");
+    res.send(text);
+  } catch (err) {
+    res.status(500).send("Proxy error: " + err.message);
+  }
+});
+
+export default app;
